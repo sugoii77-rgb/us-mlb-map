cb70f90b713767c4d3d44c90902bc3a51a36b57b
sugoii77-rgb <sugoii77@gmail.com>
2025-08-12 14:42:45 +0900

    Update mlb-data.js

--- data/mlb-data.js
+++ data/mlb-data.js
@@ -1,1 +1,86 @@
-
+const mlbData = {
+    "Arizona": {
+        "cities": }
+        ]
+    },
+    "California": {
+        "cities": },
+            { "name": "Anaheim", "lat": 33.8366, "lng": -117.9143, "teams": ["Los Angeles Angels"] },
+            { "name": "San Diego", "lat": 32.7157, "lng": -117.1611, "teams": },
+            { "name": "San Francisco", "lat": 37.7749, "lng": -122.4194, "teams": },
+            { "name": "West Sacramento", "lat": 38.5816, "lng": -121.5303, "teams": ["Athletics"] }
+        ]
+    },
+    "Colorado": {
+        "cities": }
+        ]
+    },
+    "Florida": {
+        "cities": [
+            { "name": "Miami", "lat": 25.7617, "lng": -80.1918, "teams": ["Miami Marlins"] },
+            { "name": "St. Petersburg", "lat": 27.7676, "lng": -82.6403, "teams": }
+        ]
+    },
+    "Georgia": {
+        "cities": }
+        ]
+    },
+    "Illinois": {
+        "cities": }
+        ]
+    },
+    "Maryland": {
+        "cities": }
+        ]
+    },
+    "Massachusetts": {
+        "cities": }
+        ]
+    },
+    "Michigan": {
+        "cities": }
+        ]
+    },
+    "Minnesota": {
+        "cities": }
+        ]
+    },
+    "Missouri": {
+        "cities": },
+            { "name": "St. Louis", "lat": 38.6270, "lng": -90.1994, "teams": }
+        ]
+    },
+    "New York": {
+        "cities": },
+            { "name": "Flushing", "lat": 40.7678, "lng": -73.8329, "teams": }
+        ]
+    },
+    "Ohio": {
+        "cities": },
+            { "name": "Cleveland", "lat": 41.4993, "lng": -81.6944, "teams": ["Cleveland Guardians"] }
+        ]
+    },
+    "Pennsylvania": {
+        "cities": [
+            { "name": "Philadelphia", "lat": 39.9526, "lng": -75.1652, "teams": ["Philadelphia Phillies"] },
+            { "name": "Pittsburgh", "lat": 40.4406, "lng": -79.9959, "teams": ["Pittsburgh Pirates"] }
+        ]
+    },
+    "Texas": {
+        "cities": },
+            { "name": "Houston", "lat": 29.7604, "lng": -95.3698, "teams": ["Houston Astros"] }
+        ]
+    },
+    "Washington": {
+        "cities": }
+        ]
+    },
+    "Wisconsin": {
+        "cities": }
+        ]
+    },
+    "District of Columbia": {
+        "cities": }
+        ]
+    }
+};
